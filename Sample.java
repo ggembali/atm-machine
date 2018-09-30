1288fbebd5108f3ae87c05777b49574249a31add
Ganesh Gembali <ggembali@gmail.com>
2018-09-30 10:29:56 +0530

    Implemented full logic

--- Sample.java
+++ Sample.java
@@ -12,9 +12,68 @@
         filter line numbers list by removing the lines which are just found by the last word added to set. Here we make use of the fact that both lists are sorted
   9. Repeat from step-4
   
+  Note : Two major differences compared to solution given in interview
+    - Count map is not adding value as we can remove a line even if a word occurs once
+    - Instead of picking next word having max occurences from original map, first cleaning up the map to find next word having max occurences in remaining lines
   
 */
 class MinWordFinder{
-  
+  Map<String,Set<Integer>> wordOccurences = new HashMap<>();
 
+  
+  public void findMinWordsSet(String filePath){
+  
+    InputStreamReader reader = new BufferedInputStreamReader(new FileInputStream(filePath));
+      String currentLine = reader.readLine();
+      int currentLineNumber = 0;
+      while(line != null){
+        currentLineNumber++;
+        String[] words = line.split(" ");
+        for(int i=0; i < words.length;i++){
+          if(wordOccurences.contains(words[i])){
+            wordOccurences.get(words[i]).add(currentLineNumber);
+          }else{
+            Set<Integer> lineNumbers = new SortedHashSet<>();
+            lineNumbers.add(currentLineNumber);
+            wordOccurences.put(words[i],lineNumbers);
+          }
+        }
+      }
+      Set<String> minwordsSet = new HashSet<>();
+      int totalLines = currentLineNumber;
+      int removedPages = 0;
+
+      while(removedPages.size() < totalLines){
+        String wordWithMaxOccurences = findWordInMaxLines();
+        minWordSet.add(wordWithMaxOccurences);
+        removedPages += wordOccurences.get(wordWithMaxOccurences).size());
+        filterLineNumbersFromOtherWords( wordOccurences.get(wordWithMaxOccurences));
+
+      }
+      return minWordSet;
+  }
+  
+  public void filterLineNumbersFromOtherWords(Set<Integer> linesTobeRemoved){
+    for(String word : wordOccurences.keySet()){
+      Set<Integer> lineNumbers = wordOccurences.get(word);
+      lineNumbers.removeAll(linesToBeRemoved);
+      if(lineNumbers.isEmpty()){
+        wordOccurences.remove(word);
+      }
+    }
+  }
+  
+  public String findWordInMaxLines(){
+    Set<String> words = wordOccurences.keySet();
+    int maxLines = 0;
+    String maxOccWord = null;
+    for(String word : words){
+      if(wordOccurences.get(word).size() > maxLines){
+        maxLines = wordOccurences.get(word).size();
+        maxOccWord = word;
+      }
+    }
+    return maxOccWord;
+  }
+  
 }
